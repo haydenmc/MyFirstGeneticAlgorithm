409bcd03650bbdd13551ea64456275946541c984
Hayden McAfee <hamc@microsoft.com>
2016-04-07 19:12:18 -0700

    Can generate random chromosomes

---
+++ Chromosome.cs
@@ -0,0 +1,149 @@
+using System;
+using System.Linq;
+
+namespace MyFirstGeneticAlgorithm
+{
+    public class Chromosome
+    {
+        /// <summary>
+        /// The minimum number of genes in each chromosome (will be increased to pad out to byte-even)
+        /// </summary>
+        public const int MinChromosomeLength = 6;
+        
+        /// <summary>
+        /// Random instance
+        /// </summary>
+        private static Random _random = new Random();
+        
+        /// <summary>
+        /// Set of valid gene values
+        /// </summary>
+        public static readonly char[] GeneValues
+            = new[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '*', '/' };
+        
+        /// <summary>
+        /// How many bits are required to encode a gene to binary
+        /// </summary>
+        public static int BitsPerGene
+        {
+            get
+            {
+                int i = sizeof(int) * 8; // byte size 
+                while (true)
+                {
+                    if ( (GeneValues.Length & (1 << i - 1)) != 0 )
+                    {
+                        break;
+                    }
+                    --i;
+                }
+                return i;
+            }
+        }
+        
+        /// <summary>
+        /// The size in bytes of a single chromosome
+        /// </summary>
+        public static int ChromosomeByteSize
+        {
+            get
+            {
+                var bits = MinChromosomeLength * BitsPerGene;
+                while (bits % 8 != 0)
+                {
+                    bits += BitsPerGene;
+                }
+                return (int) (bits / 8);
+            }
+        }
+        
+        /// <summary>
+        /// Gene make up of the chromosome
+        /// </summary>
+        /// <returns></returns>
+        private byte[] _genes { get; set; }
+        
+        public Chromosome()
+        {
+            // random chromosome
+            _genes = new byte[ChromosomeByteSize];
+            _random.NextBytes(_genes);
+        }
+        
+        /// <summary>
+        /// Returns a binary string representation of the chromosome
+        /// </summary>
+        public string ToBinaryString()
+        {
+            return string.Join("", _genes.Select(g => Convert.ToString(g, 2).PadLeft(8, '0')));
+        }
+        
+        /// <summary>
+        /// Returns a decoded string representation of the chromosome
+        /// </summary>
+        override public string ToString()
+        {
+            byte[] currentBuffer = new byte[(BitsPerGene / 8) + 1]; 
+            int currentByte = 0;
+            while (true)
+            {
+                int resultIndex = 0;
+                for (var i = 0; i < currentBuffer.Length; i++)
+                {
+                    currentBuffer[i] = _genes[currentByte + i];
+                }
+            }
+            throw new NotImplementedException();
+        }
+        
+        /// <summary>
+        /// Evaluate the 'equation' represented by this chromosome
+        /// </summary>
+        /// <returns>The resulting double</returns>
+        public double Evaluate()
+        {
+            throw new NotImplementedException();
+            // double? result = null;
+            // for (var i = 0; i < _genes.Length; i++)
+            // {
+            //     // If we're an operator, apply operation
+            //     if (_genes[i].IsOperator)
+            //     {
+            //         // Ensure there is a number beyond this operator we can actually use
+            //         // And ensure we have something stored for the left side of the operation
+            //         if (result != null && (i + 1) < _genes.Length && !_genes[i + 1].IsOperator)
+            //         {
+            //             // Apply the operation
+            //             switch (_genes[i].Value)
+            //             {
+            //                 case '+':
+            //                     result = result + _genes[i + 1].Value;
+            //                     break;
+            //                 case '-':
+            //                     result = result - _genes[i + 1].Value;
+            //                     break;
+            //                 case '*':
+            //                     result = result * _genes[i + 1].Value;
+            //                     break;
+            //                 case '/':
+            //                     result = result / _genes[i + 1].Value; // TODO: Catch divide by zero
+            //                     break;
+            //             }
+            //             // Skip over number
+            //             i++;
+            //             continue;
+            //         }
+            //     }
+            //     else
+            //     {
+            //         // If we have nothing stored, grab the value
+            //         if (result == null)
+            //         {
+            //             result = _genes[i].Value;
+            //         }
+            //     }
+            // }
+            // return result == null ? 0 : (double)result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace MyFirstGeneticAlgorithm
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double targetNumber = 0;
+            if (args.Length < 1 || !double.TryParse(args[0], out targetNumber))
+            {
+                Console.WriteLine("Expecting number as first argument.");
+                return;
+            }
+            Console.WriteLine($"Attempting to find equation that evaluates to {targetNumber}.");
+            Console.WriteLine($"Bits per gene: {Chromosome.BitsPerGene} bits.");
+            Console.WriteLine($"Chromosome size: {Chromosome.ChromosomeByteSize} bytes.");
+            for (var i = 0; i < 5; i++)
+            {
+                var chromosome = new Chromosome();
+                Console.WriteLine($"Random chromosome: {chromosome.ToBinaryString()}.");
+            }
+        }
+        
+        public double TargetNumber { get; private set; }
+        
+        public Program(double targetNumber)
+        {
+            TargetNumber = targetNumber;
+        }
+        
+        private double EvaluateFitness(Chromosome chromosome)
+        {
+            var chromosomeValue = chromosome.Evaluate();
+            var distance = Math.Abs(chromosomeValue - TargetNumber);
+            if (distance == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return 1 / distance;
+            }
+        }
+        
+    }
+}
